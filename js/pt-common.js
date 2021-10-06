095320ae6ed18636fc0c5ef489d55aecd144770a
kang2oon <kang2oon@gmail.com>
2021-10-06 14:08:09 +0900

    빅텍_PC및모바일

---
+++ js/pt-common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var gnbOffset = $('.com_mod_anchor').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > gnbOffset.top) {
+            $('.com_mod_header, .com_mod_anchor').addClass('fixed');
+            var headerHeight = $('.com_mod_header').height();
+            console.log(headerHeight);
+            $('.com_mod_anchor').css('top', headerHeight+48);
+        } else {
+            $('.com_mod_header, .com_mod_anchor').removeClass('fixed');
+            $('.com_mod_anchor').css('top', 0);
+        }
+    });
+});
