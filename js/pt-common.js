2f90a18d5ffe54324e1dfc15dddb0063ad8b4341
aldo814 <iko_hello@naver.com>
2021-11-03 15:19:29 +0900

    복리후생 ing

--- js/pt-common.js
+++ js/pt-common.js
@@ -47,3 +47,462 @@ $(document).ready(function(){
 		});
 
 	$(".imgFill").imgLiquid();
+<<<<<<< Updated upstream
+=======
+	
+	
+			// 조직문화&복리후생 masony 레이아웃 
+			var divs = $(".mag_mod_culture.sty03 .dvCulture .swiper-wrapper > div");
+			if ($(window).width() > 1260) {
+				for (var i = 0; i < divs.length; i += 8) {
+					divs.slice(i, i + 8).wrapAll("<div class='masony swiper-slide'></div>");
+					$('.dvCulture .swiper-wrapper > div').each(function() {
+						$(this).children().removeClass('swiper-slide');
+					});
+					$('.masony  > div').addClass('item');
+				}
+			} else {
+				$('.dvCulture .swiper-wrapper').addClass("masony")
+				$('.dvCulture .swiper-wrapper > div').each(function() {
+					$(this).addClass('item');
+				});
+			}
+
+			var ww = $(window).width();
+			var dvCultureSlider = undefined;
+
+			function initSwiper() {
+				if (ww > 1260 && dvCultureSlider == undefined) {
+					dvCultureSlider = new Swiper(".mag_mod_culture.sty03 .dvCulture", {
+						slidesPerView: 1,
+						navigation: {
+							nextEl: ".mag_mod_culture.sty03 .swiper-button-next",
+							prevEl: ".mag_mod_culture.sty03 .swiper-button-prev"
+						}
+					});
+				} else if (ww >= 1260 && dvCultureSlider != undefined) {
+					mySwiper.destroy();
+					dvCultureSlider = undefined;
+				}
+			}
+
+			initSwiper();
+
+			$(window).on('resize', function() {
+				ww = $(window).width();
+				initSwiper();
+			});
+
+			var cultureSlider = new Swiper(".mag_mod_culture.sty04 .dvCulture", {
+				slidesPerView: 4,
+				slidesPerColumn: 2,
+				slidesPerGroup: 8,
+				spaceBetween: 10,
+				navigation: {
+					nextEl: ".mag_mod_culture.sty04  .swiper-button-next",
+					prevEl: ".mag_mod_culture.sty04  .swiper-button-prev"
+				},
+				breakpoints: {
+					300: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					640: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					1024: {
+						slidesPerView: 4,
+						slidesPerColumn: 2,
+						slidesPerGroup: 8,
+					}
+				}
+			});
+
+			var cultureSlider = new Swiper(".mag_mod_culture.sty05 .dvCulture", {
+				slidesPerView: 4,
+				slidesPerColumn: 2,
+				slidesPerGroup: 8,
+				spaceBetween: 20,
+				navigation: {
+					nextEl: ".mag_mod_culture.sty05 .swiper-button-next",
+					prevEl: ".mag_mod_culture.sty05 .swiper-button-prev"
+				},
+				breakpoints: {
+					300: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					640: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					1024: {
+						slidesPerView: 4,
+						slidesPerColumn: 2,
+						slidesPerGroup: 8,
+					}
+				}
+			});
+
+			var welfareSlider = new Swiper(".bro_mod_culture.sty02 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty02 .welfare_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty02 .welfare_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".bro_mod_culture.sty02 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty02 .culture_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty02 .culture_wrap .swiper-button-prev"
+				}
+			});
+
+			var welfareSlider = new Swiper(".bro_mod_culture.sty03 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty03 .welfare_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty03 .welfare_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".bro_mod_culture.sty03 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty03 .culture_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty03 .culture_wrap .swiper-button-prev"
+				}
+			});
+
+
+			var welfareSlider = new Swiper(".bro_mod_culture.sty04 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty04 .welfare_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty04 .welfare_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".bro_mod_culture.sty04 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty04 .culture_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty04 .culture_wrap .swiper-button-prev"
+				}
+			});
+
+			$(".bro_mod_culture.sty05 .dvCulture").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".bro_mod_culture.sty05 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				pagination: {
+					el: '.dvCulture .swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+
+
+			$(".bro_mod_culture.sty05 .dvWelfare").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".bro_mod_culture.sty05 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+
+			$(".bro_mod_culture.sty06 .dvCulture").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".bro_mod_culture.sty06 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+
+			$(".bro_mod_culture.sty06 .dvWelfare").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".bro_mod_culture.sty06 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+
+			var cultureSlider = new Swiper(".bro_mod_culture.sty07 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty07 .culture_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty07 .culture_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".bro_mod_culture.sty07 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				navigation: {
+					nextEl: ".bro_mod_culture.sty07 .welfare_wrap .swiper-button-next",
+					prevEl: ".bro_mod_culture.sty07 .welfare_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".rec_mod_welfare.sty01 .welfare", {
+				slidesPerView: 3,
+				slidesPerColumn: 2,
+				slidesPerGroup: 6,
+				spaceBetween: 10,
+				navigation: {
+					nextEl: ".rec_mod_welfare.sty01  .swiper-button-next",
+					prevEl: ".rec_mod_welfare.sty01  .swiper-button-prev"
+				},
+				breakpoints: {
+					300: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					640: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					1024: {
+						slidesPerView: 3,
+						slidesPerColumn: 2,
+						slidesPerGroup: 6,
+					}
+				}
+			});
+
+			var cultureSlider = new Swiper(".rec_mod_welfare.sty03 .welfare", {
+				slidesPerView: 3,
+				slidesPerColumn: 2,
+				slidesPerGroup: 6,
+				spaceBetween: 10,
+				navigation: {
+					nextEl: ".rec_mod_welfare.sty03  .swiper-button-next",
+					prevEl: ".rec_mod_welfare.sty03  .swiper-button-prev"
+				},
+				breakpoints: {
+					300: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					640: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					1024: {
+						slidesPerView: 3,
+						slidesPerColumn: 2,
+						slidesPerGroup: 6,
+					}
+				}
+			});
+
+			var cultureSlider = new Swiper(".rec_mod_welfare.sty04 .welfare", {
+				slidesPerView: 3,
+				scrollbar: {
+					el: ".swiper-scrollbar",
+					hide: false,
+				}
+			});
+
+			var welfareSlider = new Swiper(".mag_mod_culture.sty02 .welfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 30,
+				navigation: {
+					nextEl: ".mag_mod_culture.sty02 .welfare_wrap .swiper-button-next",
+					prevEl: ".mag_mod_culture.sty02 .welfare_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".mag_mod_welfare.sty03 .dvCulture", {
+				slidesPerView: 2,
+				slidesPerColumn: 3,
+				slidesPerGroup: 6,
+				spaceBetween: 30,
+				navigation: {
+					nextEl: ".culture_wrap .swiper-button-next",
+					prevEl: ".culture_wrap .swiper-button-prev"
+				},
+				breakpoints: {
+					300: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					640: {
+						slidesPerView: 1,
+						slidesPerColumn: 3,
+						slidesPerGroup: 3,
+					},
+					1024: {
+						slidesPerView: 2,
+						slidesPerColumn: 3,
+						slidesPerGroup: 6,
+					}
+				}
+			});
+
+			var cultureSlider = new Swiper(".mag_mod_culture.sty02 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				navigation: {
+					nextEl: ".culture_wrap .swiper-button-next",
+					prevEl: ".culture_wrap .swiper-button-prev"
+				}
+			});
+
+			$(".inf_mod_culture.sty01 .dvCulture").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".inf_mod_culture.sty01 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+
+			$(".inf_mod_culture.sty01 .dvWelfare").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".inf_mod_culture.sty01 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+
+			$(".inf_mod_culture.sty01 .welfare").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".inf_mod_culture.sty01 .welfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+			
+			var welfareSlider = new Swiper(".sns_mod_culture.sty02 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				spaceBetween: 30,
+				navigation: {
+					nextEl: ".sns_mod_culture.sty02 .welfare_wrap .swiper-button-next",
+					prevEl: ".sns_mod_culture.sty02 .welfare_wrap .swiper-button-prev"
+				}
+			});
+
+			var cultureSlider = new Swiper(".sns_mod_culture.sty02 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 1,
+				spaceBetween: 30,
+				navigation: {
+					nextEl: ".sns_mod_culture.sty02 .culture_wrap .swiper-button-next",
+					prevEl: ".sns_mod_culture.sty02 .culture_wrap .swiper-button-prev"
+				}
+			});
+			
+			$(".inf_mod_culture.sty02 .dvCulture").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".inf_mod_culture.sty02 .dvCulture", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+			
+			$(".inf_mod_culture.sty02 .dvWelfare").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".inf_mod_culture.sty02 .dvWelfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+			
+			$(".inf_mod_culture.sty02 .welfare").append('<div class="swiper-pagination"></div>');
+			var cultureSlider = new Swiper(".inf_mod_culture.sty02 .welfare", {
+				slidesPerView: 1,
+				slidesPerColumn: 3,
+				spaceBetween: 10,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					clickable: true,
+					observer: true,
+					observeParents: true,
+				}
+			});
+			
+			// 탭
+			$('.conts > div').hide();
+			$('.conts > div').first().show();
+			$('.tab > a').click(function() {
+				var item = $(this).attr('data-tab');
+				$('.tab > a').removeClass('on');
+				$(this).addClass('on');
+				$('.conts > div').hide();
+				$('.conts > .' + item).show();
+			});
+
+	
+	
+});
+>>>>>>> Stashed changes
